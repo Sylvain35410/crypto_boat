2f3f773b39ed52ed32feb68186ca221aa10a759a
Sylvain <>
2024-09-30 17:17:43 +0200

    Simplication des taches pour éviter un échange de données trés volumineux

--- dags/train_model_dag.py
+++ dags/train_model_dag.py
@@ -23,58 +23,21 @@ with DAG(
     schedule_interval=None,  # Exécution temporairement manuelle
 ) as dag:
 
-    # Groupe de tâches pour préparer les données d'entraînement
-    with TaskGroup("prepare_data_group") as prepare_data_group:
-        
-        def prepare_data(symbol):
-            from scripts.train_model import load_training_data
-            load_training_data(symbol, "15m")
+    def train_crypto_model(symbol, interval):
+        from scripts.train_model import training
+        training(symbol, interval)
 
-        # Préparer les données pour BTC et ETH
-        prepare_btc_data_task = PythonOperator(
-            task_id='prepare_btc_training_data',
-            python_callable=lambda: prepare_data("BTCUSDT")
-        )
+    train_btc_model_task = PythonOperator(
+        task_id='train_btc_model',
+        python_callable=train_crypto_model,
+        op_kwargs={'symbol': 'BTCUSDT', 'interval': '15m'}
+    )
 
-        prepare_eth_data_task = PythonOperator(
-            task_id='prepare_eth_training_data',
-            python_callable=lambda: prepare_data("ETHUSDT")
-        )
-
-    # Groupe de tâches pour entraîner le modèle
-    with TaskGroup("train_model_group") as train_model_group:
-
-        def train_crypto_model(symbol):
-            from scripts.train_model import train_model
-            train_model(symbol, "15m")
-
-        train_btc_model_task = PythonOperator(
-            task_id='train_btc_model',
-            python_callable=lambda: train_crypto_model("BTCUSDT")
-        )
-
-        train_eth_model_task = PythonOperator(
-            task_id='train_eth_model',
-            python_callable=lambda: train_crypto_model("ETHUSDT")
-        )
-
-    # Groupe de tâches pour sauvegarder le modèle entraîné
-    with TaskGroup("save_model_group") as save_model_group:
-
-        def save_model(symbol):
-            from scripts.train_model import train_and_save_model
-            train_and_save_model(symbol, "15m")
-
-        save_btc_model_task = PythonOperator(
-            task_id='save_btc_trained_model',
-            python_callable=lambda: save_model("BTCUSDT")
-        )
-
-        save_eth_model_task = PythonOperator(
-            task_id='save_eth_trained_model',
-            python_callable=lambda: save_model("ETHUSDT")
-        )
+    train_eth_model_task = PythonOperator(
+        task_id='train_eth_model',
+        python_callable=train_crypto_model,
+        op_kwargs={'symbol': 'ETHUSDT', 'interval': '15m'}
+    )
 
     # Ordonnancement des tâches
-    prepare_btc_data_task >> train_btc_model_task >> save_btc_model_task
-    prepare_eth_data_task >> train_eth_model_task >> save_eth_model_task
+    train_btc_model_task >> train_eth_model_task
